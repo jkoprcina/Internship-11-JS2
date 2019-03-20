6f2a42125807e1121f528cf670235d4f5e363957
jkoprcina <koprcina.josip@gmail.com>
2019-03-20 09:28:23 +0100

    Feel lines more changed

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -17,30 +17,29 @@ let filterArraySleeveLength = [
   { type: "Medium", count: "65" }
 ];
 
-let arrays = [ filterArraySort, filterArrayProductType, filterArraySleeveLength ]
-//Koristim niz u nizu umisto objecta zbog lakse iteracije kroz njega 
+let arrays = [filterArraySort, filterArrayProductType, filterArraySleeveLength];
+//Koristim niz u nizu umisto objecta zbog lakse iteracije kroz njega
 
 let allDropdownLists = document.querySelectorAll(".dropdown__list");
-let elementToAdd = "";
 arrays.forEach(function(array, index) {
   array.forEach(function(item) {
-      elementToAdd = document.createElement("li");
-      elementToAdd.innerHTML =item.type + " - " + item.count;
-      elementToAdd.classList.add("dropdown__list__item");
-      allDropdownLists[index].appendChild(elementToAdd);
+    let elementToAdd = document.createElement("li");
+    elementToAdd.innerHTML = item.type + " - " + item.count;
+    elementToAdd.classList.add("dropdown__list__item");
+    allDropdownLists[index].appendChild(elementToAdd);
   });
 });
 
 //SECOND TASK
 let arrayInfo = [
-  {description: "Long", paragraph: "A very long shirt", price: "12.3£"},
-  {description: "Short", paragraph: "A very short shirt", price: "19£"},
-  {description: "Black", paragraph: "A very black shirt", price: "5.2£"},
-  {description: "Small", paragraph: "A very small shirt", price: "42.1£"},
-  {description: "WTF", paragraph: "A very WTF shirt", price: "32.5£"},
-  {description: "Red", paragraph: "A very red shirt", price: "333£"},
-  {description: "Blue", paragraph: "A very blue shirt", price: "45.2£"},
-  {description: "Green", paragraph: "A very green shirt", price: "FREE"}
+  { description: "Long", paragraph: "A very long shirt", price: "12.3£" },
+  { description: "Short", paragraph: "A very short shirt", price: "19£" },
+  { description: "Black", paragraph: "A very black shirt", price: "5.2£" },
+  { description: "Small", paragraph: "A very small shirt", price: "42.1£" },
+  { description: "WTF", paragraph: "A very WTF shirt", price: "32.5£" },
+  { description: "Red", paragraph: "A very red shirt", price: "333£" },
+  { description: "Blue", paragraph: "A very blue shirt", price: "45.2£" },
+  { description: "Green", paragraph: "A very green shirt", price: "FREE" }
 ];
 let whereToAddElements = document.querySelector(".main__offers");
 
@@ -52,7 +51,7 @@ arrayInfo.forEach(function(item) {
     <p class="item__img-description">${item.description}</p>
     <p class="item__paragraph">${item.paragraph}</p>
     <span class="item__price">${item.price}</span>
-  </div>`
+  </div>`;
 });
 
 //THIRD, FOURTH && FIFTH TASK
@@ -60,6 +59,8 @@ let arrayOfItems = document.querySelectorAll(".offers__item");
 let arrayOfDescriptions = document.querySelectorAll(".item__img-description");
 let arrayOfHearts = document.querySelectorAll(".item__heart-full");
 let arrayOfImg = document.querySelectorAll(".item__img");
+
+//Used in manipulating span function
 let main = document.querySelector("main");
 let mainDiv = document.querySelector(".main__offers");
 let favouritesCounterSpan = document.createElement("span");
@@ -69,48 +70,48 @@ arrayOfHearts.forEach(function(item) {
   item.clicked = false;
 });
 
-for (let i = 0; i < arrayOfItems.length; i++) {
+arrayOfItems.forEach(function(item, index) {
   //THIRD TASK
-  arrayOfItems[i].addEventListener("mouseover", function() {
-    arrayOfDescriptions[i].style.opacity = 1;
-    arrayOfDescriptions[i].style.top = "297px";
-    arrayOfHearts[i].style.opacity = 1;
+  item.addEventListener("mouseover", function() {
+    arrayOfDescriptions[index].style.opacity = 1;
+    arrayOfDescriptions[index].style.top = "297px";
+    arrayOfHearts[index].style.opacity = 1;
   });
 
-  arrayOfItems[i].addEventListener("mouseleave", function() {
-    arrayOfDescriptions[i].style.opacity = 0;
-    if (!arrayOfHearts[i].clicked) {
-      arrayOfHearts[i].style.opacity = 0;
+  item.addEventListener("mouseleave", function() {
+    arrayOfDescriptions[index].style.opacity = 0;
+    if (!arrayOfHearts[index].clicked) {
+      arrayOfHearts[index].style.opacity = 0;
     }
   });
 
   //FOURTH TASK
-  arrayOfHearts[i].addEventListener("click", function(event) {
-    if (arrayOfHearts[i].clicked) {
-      arrayOfHearts[i].style.opacity = 0;
-      arrayOfHearts[i].clicked = false;
+  arrayOfHearts[index].addEventListener("click", function(event) {
+    if (arrayOfHearts[index].clicked) {
+      arrayOfHearts[index].style.opacity = 0;
+      arrayOfHearts[index].clicked = false;
       numberOfFavourites--;
     } else {
-      arrayOfHearts[i].style.opacity = 1;
-      arrayOfHearts[i].clicked = true;
+      arrayOfHearts[index].style.opacity = 1;
+      arrayOfHearts[index].clicked = true;
       numberOfFavourites++;
     }
-    //method manipulateSpan is at the bottom 
+    //method manipulateSpan is at the bottom
     manipulateSpan();
     event.stopPropagation();
   });
 
   //FIFTH TASK
-  arrayOfItems[i].addEventListener("click", function() {
+  item.addEventListener("click", function() {
     let newDetailsPage = window.open();
     newDetailsPage
       .document.write(`<link rel="stylesheet" href="./assets/styles/main.css"/>
                 <body><main><div class="main__offers"> ${
-                  arrayOfItems[i].outerHTML
+                  item.outerHTML
                 } </div></main></body>`);
     newDetailsPage.document.close();
   });
-}
+});
 
 //adds/removes span or changes it's context, whichever is needed
 function manipulateSpan() {
